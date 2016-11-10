4eff5fc9f578ba4830bb4a0228770811ecc87702
TYzzt <378741605@qq.com>
2016-11-10 11:27:06 +0800

    模拟登陆

---
+++ src/ImageUtil/RecogniseMain.java
@@ -0,0 +1,14 @@
+package ImageUtil;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: ZhaoTao
+ * \* Date: 2016/11/10
+ * \* Time: 10:54
+ * \* Description:
+ * \
+ */
+public class RecogniseMain {
+
+
+}
